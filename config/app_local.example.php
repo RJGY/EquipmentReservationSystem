7126b1b98ec0d9e44052aa6d4c99b96822347ebc
PierceKH <87044056+AllPurple@users.noreply.github.com>
2021-07-30 03:35:19 +1000

    initial database connection commit, more work needs to be done

--- config/app_local.example.php
+++ config/app_local.example.php
@@ -36,6 +36,9 @@ return [
      */
     'Datasources' => [
         'default' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => false,
             'host' => 'localhost',
             /*
              * CakePHP will use the default DB port based on the driver selected
@@ -44,10 +47,8 @@ return [
              */
             //'port' => 'non_standard_port_number',
 
-            'username' => 'my_app',
-            'password' => 'secret',
-
-            'database' => 'my_app',
+            'username' => 'root',
+            'database' => 'cqulabmanager',
             /*
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
